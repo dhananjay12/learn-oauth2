a3ce0dae1d6f3d1926101f1a53e372814a82ab55
dhananjay12 <dhananjay1204@gmail.com>
2020-09-27 14:27:22 +0200

    resource server

---
+++ resource-server/src/main/java/com/djcodes/spring/resourceserver/controller/SimpleController.java
@@ -0,0 +1,23 @@
+package com.djcodes.spring.resourceserver.controller;
+
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.security.oauth2.jwt.Jwt;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/simple")
+public class SimpleController {
+
+    @GetMapping("/check")
+    public String status() {
+        return "Working...";
+    }
+
+    @GetMapping("/jwt")
+    public Jwt jwt(@AuthenticationPrincipal Jwt jwt) {
+        return jwt;
+    }
+
+}
---
+++ resource-server/src/test/java/com/djcodes/spring/resourceserver/ResourceServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.djcodes.spring.resourceserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ResourceServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
