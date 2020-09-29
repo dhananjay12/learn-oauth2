6686f5d06538a5965d48ff0f7daa16d5e60ad7d3
dhananjay12 <dhananjay1204@gmail.com>
2020-09-29 11:32:15 +0200

    Method level security

---
+++ resource-server/src/main/java/com/djcodes/spring/resourceserver/controller/MethodLvlSecurityController.java
@@ -0,0 +1,56 @@
+package com.djcodes.spring.resourceserver.controller;
+
+import java.util.Map;
+import org.springframework.security.access.annotation.Secured;
+import org.springframework.security.access.prepost.PostAuthorize;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.security.oauth2.jwt.Jwt;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/method")
+public class MethodLvlSecurityController {
+
+    @Secured("ROLE_beta_user")
+    @GetMapping("/secured")
+    public String secured() {
+        System.out.println("In Secured");
+        return "Secured annotation working";
+    }
+
+    @PreAuthorize("hasAuthority('ROLE_beta_user') or #username == #jwt.claims.get('preferred_username')")
+    @GetMapping("/pre-authorize/{username}")
+    public String preAuthorize(@PathVariable String username, @AuthenticationPrincipal Jwt jwt) {
+        System.out.println("In PreAuthorize");
+        return "PreAuthorize annotation working";
+    }
+
+    @PostAuthorize("returnObject.something == #jwt.claims.get('preferred_username')")
+    @GetMapping("/post-authorize/{username}")
+    public SomeObject postAuthorize(@PathVariable String username, @AuthenticationPrincipal Jwt jwt) {
+        System.out.println("In PostAuthorize");
+        return new SomeObject(username);
+    }
+
+}
+
+class SomeObject {
+
+    private String something;
+
+    public SomeObject(String something) {
+        this.something = something;
+    }
+
+    public String getSomething() {
+        return something;
+    }
+
+    public void setSomething(String something) {
+        this.something = something;
+    }
+}
