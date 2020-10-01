3143d0abd35e644d2b3a8ab39532b11667bce191
dhananjay12 <dhananjay1204@gmail.com>
2020-10-01 17:41:10 +0200

    eureka

--- reactive-resource-server/src/main/java/com/djcodes/spring/oauth2/reactiveresourceserver/ReactiveResourceServerApplication.java
+++ reactive-resource-server/src/main/java/com/djcodes/spring/oauth2/reactiveresourceserver/ReactiveResourceServerApplication.java
@@ -2,8 +2,10 @@ package com.djcodes.spring.oauth2.reactiveresourceserver;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
+@EnableDiscoveryClient
 public class ReactiveResourceServerApplication {
 
 	public static void main(String[] args) {
